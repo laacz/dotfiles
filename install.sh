4f5c3ceba26200bd41580d040ca4e1faee0494b6
Kaspars Foigts <laacz@laacz.lv>
2022-10-30 00:30:35 +0300

    fix: zshrc file was not symlinked

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 # Hide "last login" line when starting a new terminal session
 touch $HOME/.hushlogin
 
-# Install omz
-rm -rf $HOME/.oh-my-zsh $HOME/.p10k.zsh
+# Install omza
+rm -rf $HOME/.oh-my-zsh $HOME/.p10k.zsh $HOME/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k
 
