bb8e3ae953e384307469fbb53a85e7df2f5aec66
Kaspars Foigts <laacz@laacz.lv>
2025-06-23 11:53:47 +0300

    install update

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ touch $HOME/.hushlogin
 # Install omza
 rm -rf $HOME/.oh-my-zsh
 #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no CHSH=no sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 rm -rf $HOME/.p10k.zsh
